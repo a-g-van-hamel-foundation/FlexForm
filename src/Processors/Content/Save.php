3700885744b8ed627576e36510278393b2e94a99
D-Groenewegen <dennisgroenewegen@gmail.com>
2026-08-13 21:48:50 +0200

    jimmy-fix

--- src/Processors/Content/Save.php
+++ src/Processors/Content/Save.php
@@ -14,7 +14,6 @@ use MediaWiki\MediaWikiServices;
 use MediaWiki\Revision\SlotRecord;
 use MWContentSerializationException;
 use RequestContext;
-use SMW\Maintenance\DataRebuilder;
 use SMW\Services\ServicesFactory;
 use Title;
 use User;
@@ -319,8 +318,7 @@ class Save {
 			$store->setOption( Store::OPT_CREATE_UPDATE_JOB,
 				false );
 
-			$rebuilder = new DataRebuilder( $store,
-				ServicesFactory::getInstance()->newTitleFactory() );
+			$rebuilder = ServicesFactory::getInstance()->getMaintenanceFactory()->newDataRebuilder( $store );
 
 			$rebuilder->setOptions( // Tell SMW to only rebuild the current page
 				new Options( [ 'page' => $title->getFullText() ] ) );
